d21dfacad6601b441fc338c617f2519c0b2e1aa6
Dani Tallón <kalimero99@protonmail.ch>
2023-04-12 13:56:14 +0200

    manually test footer a header display correctly

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -35,9 +35,8 @@ const count = ref(0)
     <div>
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
-
-      <router-view></router-view>
     </div>
+    
 </template>
 
 <style scoped>
