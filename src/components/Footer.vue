ceba5134ff9bdfacf240c13e9a05ef15629f7dfa
Dani Tallón <kalimero99@protonmail.ch>
2023-04-12 13:54:39 +0200

    modify Footer and Header to be truly sticky

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class=" fixed bottom-0 left-0 w-full">
+        <hr>
+        <div class="flex justify-center">
+            This is the footer
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class=" fixed top-0 left-0 w-full">
+        <div class="flex justify-center">
+            This is the header
+        </div>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
