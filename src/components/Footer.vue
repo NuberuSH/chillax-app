90ec07d46b3a845fd0e7debf5ffd858262e9c6f9
Dani Tallón <kalimero99@protonmail.ch>
2023-04-13 23:52:10 +0200

    add API disclaimer to footer (not styled yet)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class=" fixed bottom-0 left-0 w-full">
+    <div class="flex flex-col md:flex-row fixed bottom-0 left-0 w-full bg-gray-900 opacity-60">
         <hr>
-        <div class="flex justify-center">
-            This is the footer
-        </div>
+          <p>© CHILLAX 2023</p>
+          <p class="api-disclaimer">This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
+          <img src="/images/logo/TMDB_long.svg" alt="Logo de TMDB" class="api-logo">
     </div>
 </template>
 
