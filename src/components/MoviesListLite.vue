735eeb4e00b5c47057be05ff4267341edb4364bb
Dani Tallón <kalimero99@protonmail.ch>
2023-04-17 16:03:41 +0200

    create MoviesListsLite component and import in router

---
+++ src/components/MoviesListLite.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+       <div id="series-list">
+         <router-link v-for="movie in moviesList" :key="movie.id" class="movie" :to="{ name: 'home', params: {id: movie.id}}">
+           <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="`Poster of the movie: ${movie.name}`">
+           <div class="movie-footer">
+             <div class="title">{{ movie.title }}</div>
+           </div>
+         </router-link>
+       </div>
+    </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+export default {
+    setup() {
+        const moviesList = ref([]);
+        const page = ref()
+        onMounted(async () => {
+            try {
+                const response = await axios.get('/.netlify/functions/getMovies', {
+                    params: {
+                        page: page.value,
+                    }
+                });
+                page.value = response.data.page;
+                moviesList.value = response.data.results.slice(0, 6);
+            } catch (error) {
+                console.error(error);
+            }
+        });
+
+        return {
+            moviesList,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
