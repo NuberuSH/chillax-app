a6ee12fdd8be783334074cd85faaad1f912106cc
Dani Tallón <kalimero99@protonmail.ch>
2023-04-18 04:04:32 +0200

    genre selection and reload batch on genre selection

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,15 +1,45 @@
 <template>
-    <div>
-<MoviesList></MoviesList>
+    <div class="mt-36">
+        <select v-model="selectedGenre">
+            <option selected value="">Selecciona un género</option>
+            <option value="">Todos</option>
+            <option v-for="genre in genreList" :value="genre.id" :key="genre.id" >
+            {{ genre.name }}
+            </option>
+        </select>
+        <MoviesList :selectedGenre="selectedGenre"></MoviesList>
     </div>
 </template>
 
 <script>
+import { ref, onMounted, watch } from 'vue';
+import axios from 'axios';
 import MoviesList from '../components/MoviesList.vue';
 
 export default {
     setup() {
-        return {};
+        const genreList = ref([]);
+        const selectedGenre = ref('');
+         watch(selectedGenre, () => {
+            console.log('debería hacer la petición', selectedGenre.value)
+        })
+        onMounted(async () => {
+            try {
+                const response = await axios.get('/.netlify/functions/getGenres', {
+                    params: {
+                        contentType : 'movie',
+                    }
+                });
+                genreList.value = response.data.genres.slice();
+            } catch (error) {
+                console.error(error);
+            }
+        });
+
+        return {
+            genreList,
+            selectedGenre
+        };
     },
     components: { MoviesList }
 }
