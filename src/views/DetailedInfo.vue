e2ce2567fba0a6f95d27b574a436c49c2927c84d
Carla G <carla.gomez.abad@gmail.com>
2023-04-18 16:15:43 +0200

    vanilla info card turned into a component, css to tailwind

--- src/views/DetailedInfo.vue
+++ src/views/DetailedInfo.vue
@@ -1,14 +1,17 @@
 <template>
-    <div class="movie-info-container">
-        <div class='go-back' @click="returnToList()">Volver atrás</div>
-        <h1 class="movie-h1">{{ movieData.title }}</h1>
-        <hr/>
-        <div class="small-container">
-          <img :src="'https://image.tmdb.org/t/p/w500' + movieData.poster_path" :alt="'Portada de la película ' + movieData.title" id="movie-poster">
-          <div id="info-body">
-            <div class="additional-info">
-              <p class="release-date">Fecha de estreno: {{ movieData.release_date }}</p>            
+    <div class="boxShadow w-4/5 mt-36 mb-36 mx-auto rounded-xl bg-web-card text-white shadow-3xl flex flex-col self-center sm:h-fit">
+        <div class='go-back hover:bg-web-boton2 w-fit py-1 px-2 text-xl cursor-pointer rounded-md inline-block shadow-lg ml-4 mt-4' @click="returnToList()">Volver atrás</div>
+        <h1 class="text-3xl text font-semibold self-center mb-4 md:mb-6 mt-7 md:mt-0 mx-1">{{ movieData.title }}</h1>
+        <hr class="w-4/5 self-center"/>
+        <div class="flex flex-col md:flex-row ml-0 md:ml-10 justify-center mt-4">
+          <img :src="'https://image.tmdb.org/t/p/w500' + movieData.poster_path" :alt="'Portada de la película ' + movieData.title" class="mt-5 mb-4 w-1/2 h-full md:w-1/5 md:h-1/4 rounded-lg self-center md:self-center sm:mb-7">
+          <div id="info-body" class="flex flex-col ml-0 sm:ml-20 md:ml-10 self-center w-4/5 md:w-3/5 md:text-left sm:text-center md:text-xl">
+            <div class="additional-info self-center w-4/5 h-max-content md:mb-3.5">
+              <p class="self-end w-400 leading-230">Fecha de estreno: {{ movieData.release_date }}</p>
+              <p class="runtime">Duración: {{movieData.runtime}} minutos</p>
             </div>
+            <div class="sinopsis md:mx-auto md:w-4/5 leading-230 md:mt-2/5 sm:w-4/5 mt-5 sm:self-start ml-0 mb-8">{{movieData.overview}}
+            </div>      
           </div>
         </div>
     </div>
@@ -35,9 +38,9 @@
         .then(data => {
           movieData.value = data;     
           const additionalInfo = document.getElementsByClassName('additional-info')[0];
-          if (data.runtime > 0) {
+          /*if (data.runtime > 0) {
             additionalInfo.innerHTML += `<p class="runtime">Duración: ${data.runtime} minutos</p>`;
-          }
+          }*/
           const genres = document.createElement('p');
           genres.className = 'genres';
           genres.innerText = 'Género/s: ';
@@ -61,7 +64,7 @@
           }
           additionalInfo.appendChild(networks);
           const infoBody = document.getElementById("info-body");
-          infoBody.innerHTML += `<div class="sinopsis">${data.overview}</div>`;
+          //infoBody.innerHTML += `<div class="sinopsis">${data.overview}</div>`;
         })
         .catch(error => {
           console.error(error);
@@ -69,25 +72,7 @@
       }
   
       const returnToList = () => {
-        page = 1;
-        const container = document.getElementById('container');
-        container.className = '';
-        container.innerHTML = `
-          <h2 class="section-title">Películas</h2>
-          <hr>
-          <div id="movie-list">
-  
-          </div>
-          <div class="show-more" onclick="redirectToMovies()">Mostrar más películas</div>
-          <div id="series">
-            <h2 class="section-title">Series</h2>
-            <hr>
-            <div id="series-list">
-  
-          </div>
-          <div class="show-more" onclick="redirectToSeries()">Mostrar más series</div>`;
-        // getGenres();
-        getContent();
+
       }
   
       return {
@@ -100,5 +85,11 @@
   </script>
   
   <style scoped>
-    @import "../assets/catalog.css";
+    /*@import "../assets/catalog.css";*/
+    .boxShadow {
+  box-shadow: inset 0 0 3em 3em rgb(5 0 0 / 26%), 0 0 20px rgb(29 29 29 / 60.5%), 1em 1em 10em rgb(255 255 255 / 29.9%);
+  border: 2px solid #383737;
+  
+}
+
   </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,26 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        web: {
+          boton: "#b41c10",
+          boton2: "#f37373",
+          fondo: "#C9D5E0",
+          topBar: "#4FA9A9",
+          loginButton: "#A5E1AD",
+          formBgDarkMode: "#1b1b32",
+          custom1: "#21094E",
+          custom2: "#511281",
+          custom3: "#4CA1A3",
+          custom4: "#A5E1AD",
+          buttonBack: "#353434",
+          card: "#4e4e4e",
+          shadowCard: "#3d3c3c",
+          navbar: "#510d0d"
+        }
+      },
+    },
   },
   variants: {
     extend: {},
