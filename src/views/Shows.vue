56ce53bc00f3bb8f49ef5acd9f208e9790fcfc87
Dani Tallón <kalimero99@protonmail.ch>
2023-04-19 02:48:30 +0200

    add genre filter functionality

--- src/views/Shows.vue
+++ src/views/Shows.vue
@@ -1,17 +1,49 @@
 <template>
-    <div>
-<TvShowsList></TvShowsList>
+    <div class="mt-36">
+        <select v-model="selectedGenre">
+            <option selected disabled hidden value="">Selecciona un género</option>
+            <option value="">Todos</option>
+            <option v-for="genre in genreList" :value="genre.id" :key="genre.id" >
+                {{ genre.name }}
+             </option>
+        </select>
+        <TvShowsList :selectedGenre="selectedGenre"></TvShowsList>
+        <ScrollTopButton></ScrollTopButton>
     </div>
 </template>
 
 <script>
+import { ref, onMounted, watch } from 'vue';
+import axios from 'axios';
 import TvShowsList from '../components/TvShowsList.vue';
+import ScrollTopButton from '../components/ScrollTopButton.vue';
 
 export default {
     setup() {
-        return {};
+        const genreList = ref([]);
+        const selectedGenre = ref('');
+        watch(selectedGenre, () => {
+            console.log('debería hacer la petición', selectedGenre.value)
+        })
+        onMounted(async () => {
+            try {
+                const response = await axios.get('/.netlify/functions/getGenres', {
+                    params: {
+                        contentType: 'tv',
+                    }
+                });
+                genreList.value = response.data.genres.slice();
+            } catch (error) {
+                console.error(error);
+            }
+        });
+
+        return {
+            genreList,
+            selectedGenre
+        };
     },
-    components: { TvShowsList }
+    components: { TvShowsList, ScrollTopButton }
 }
 </script>
 
