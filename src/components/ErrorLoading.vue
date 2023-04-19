2aaae3c8d89e66d901b028a3934e3f6822097e35
Dani Tallón <kalimero99@protonmail.ch>
2023-04-20 01:40:58 +0200

    create error when loading page

---
+++ src/components/ErrorLoading.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="h-screen w-screen bg-gray-900 opacity-75 text-white p-4 flex flex-col justify-center">
+        <h1>Se ha producido un error al cargar esta página</h1>
+        <h2>¿Estás seguro de que la dirección es correcta?</h2>
+        <img src="/images/icons/warning.svg" class="h-32 w-32 mx-auto mt-5">
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
