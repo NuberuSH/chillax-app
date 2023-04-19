d2c78368c27e7e505596701a2bd8072df590b9c3
Dani Tallón <kalimero99@protonmail.ch>
2023-04-19 02:38:58 +0200

    change opacity when scrolled

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="flex flex-col justify-center items-center fixed bottom-0 left-0 w-full max-h-11 bg-gray-900 opacity-50 font-montserrat text-xs font-light text-white">
+    <div class="flex flex-col justify-center items-center fixed bottom-0 left-0 w-full max-h-11 bg-gray-900 font-montserrat text-xs font-light text-white transition duration-300"
+         :class="{
+            'opacity-60': scrolledY > 0,
+            'opacity-100': scrolledY === 0
+            }">
         <hr class="w-full m-0 p-0">
           <p class="m-0 text-[0.6rem] ">© CHILLAX 2023</p>
           <p class="api-disclaimer text-[0.5rem] transition duration-300 " :class="{
