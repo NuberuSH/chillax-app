a9119076570c058f6448128b9c3d0ad6b9870d9c
Dani Tallón <kalimero99@protonmail.ch>
2023-04-19 19:13:32 +0200

    fix styles in 404 page

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -12,7 +12,7 @@
               class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-web-boton group-hover:translate-y-0 group-hover:translate-x-0"
             ></span>
 
-            <span class="relative block px-8 py-3 bg-gray-900 border border-current">
+            <span class="relative block px-8 py-3 bg-gray-900 hover:bg-gray-600 border border-current">
               Volver al inicio
             </span>
           </a>
