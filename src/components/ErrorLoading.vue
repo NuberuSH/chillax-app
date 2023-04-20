da1e15a2b618e33af46e0dec4a8ed44900b2c8f9
Dani Tallón <kalimero99@protonmail.ch>
2023-04-20 13:27:23 +0200

    fix styles bug in Error screen

--- src/components/ErrorLoading.vue
+++ src/components/ErrorLoading.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-screen w-screen bg-gray-900 opacity-75 text-white p-4 flex flex-col justify-center">
+    <div class="fixed top-0 left-0 h-screen w-screen bg-gray-900 opacity-75 text-white p-4 flex flex-col justify-center">
         <h1>Se ha producido un error al cargar esta página</h1>
         <h2>¿Estás seguro de que la dirección es correcta?</h2>
         <img src="/images/icons/warning.svg" class="h-32 w-32 mx-auto mt-5">
